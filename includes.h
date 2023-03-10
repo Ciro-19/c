6f708d4f8221cb228d2e63b189f6981feb6c8b26
Ciro-19 <91888657+Ciro-19@users.noreply.github.com>
2023-03-10 04:02:48 +0100

    Update includes.h

--- includes.h
+++ includes.h
@@ -5,21 +5,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-//void my_putnbr(int nb);=)
-// void my_putchar(char c);
 void my_putstr(char *str);
-// int my_strlen(char *str);
 int my_atoi(char *str);
 
-#define VRAIS 1
-#define FAUX 0
-
-typedef int BOOLEAN;
-
-typedef struct mystruct{
-    int minutes;
-    int hours;
-}   type_struct;
+#define TRUE 1
+#define FALSE 0
 
 typedef struct my_list {
     int nbr;
